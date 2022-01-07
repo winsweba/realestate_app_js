cec110ff71cd5ffab3356d4bdbbcf4c8525f28c1
winsweba <winsweb@outlook.com>
2022-01-07 13:57:06 -0800

    commit 3

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,9 @@
-import {Box} from '@chakra-ui/react'
-
+import { Box } from '@chakra-ui/layout';
 
 const Footer = () => (
-    <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor="gray.100">
-        2022 Win Real, Inco.
-    </Box>
-)
+  <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
+    © 2021 Realtor, Inc.
+  </Box>
+);
 
 export default Footer;
